93731062c5912e3c155b6e3ecc2735eeb7e7cd47
Sári András <sari.andras@students.jedlik.eu>
2022-09-16 11:22:09 +0200

    Reszponzív navbar kész

---
+++ js/script.js
@@ -0,0 +1,8 @@
+function navbar() {
+    var nav = document.getElementById("myTopnav");
+    if (nav.className === "topnav") {
+      nav.className += " responsive";
+    } else {
+      nav.className = "topnav";
+    }
+  }
